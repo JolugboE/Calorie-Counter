f2680f941a76205abd2aef2d162bf8f6dacd1afd
JolugboE <jolugboesther@gmail.com>
2025-02-08 23:21:17 +0100

    I'm getting better everyday

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ return str.match(regex);
   function calculateCalories(e){
     e.preventDefault();
     isError = false;
-    
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
   }
 
   function getCaloriesFromInputs(list) {
